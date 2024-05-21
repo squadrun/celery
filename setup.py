e8b0620824e25f2eb4f7f91a1a1629167ee34da7
Ayush <ayush@squadstack.com>
2024-05-22 02:50:54 +0530

    fix: patch celery 4.4.7 for CVE-2021-23727

--- setup.py
+++ setup.py
@@ -203,7 +203,7 @@ setuptools.setup(
     url=meta['homepage'],
     license='BSD',
     platforms=['any'],
-    install_requires=install_requires(),
+    install_requires=[],
     python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*",
     tests_require=reqs('test.txt'),
     extras_require=extras_require(),
